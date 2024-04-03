a96150e0d17b0d75b495d9dcdc9fa7d8dacaeae6
kamalsirssjcoe <140489074+kamalsirssjcoe@users.noreply.github.com>
2024-04-03 17:57:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const http = require("http");
 const url = require("url");
 
 const server = http.createServer( (req, res)=> {
- res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');let path = url.parse(req.url, true);
-
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 if (path.pathname == "/find")
 {
 	let num = path.query.number;
