6577d7d3813f74392e2d87cc6f5218e9f36ba289
kamalsirssjcoe <140489074+kamalsirssjcoe@users.noreply.github.com>
2024-04-03 18:01:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const server = http.createServer( (req, res)=> {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+
+let path = url.parse(req.url, true);
 if (path.pathname == "/find")
 {
 	let num = path.query.number;
